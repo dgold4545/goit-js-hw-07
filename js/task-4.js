be9f8d353d38a88f5364ae666f84c87bbc965bd2
DGOLD7788 <80936904545dgold@gmail.com>
2024-05-20 21:21:43 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,28 @@
+const loginFormElem = document.querySelector('.login-form');
 
+// console.log(loginFormElem);
+
+loginFormElem.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+  // console.log(email);
+  // console.log(password);
+  // console.log(email.value);
+  // console.log(password.value);
+  // console.log(email.value.trim().length);
+  // console.log(password.value.trim().length);
+
+  if (password.value.trim() === '' || email.value.trim() === '') {
+    alert('All form fields must be filled in');
+  }
+
+  const userLoginInformation = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  console.log(userLoginInformation);
+
+  event.currentTarget.reset();
+});
